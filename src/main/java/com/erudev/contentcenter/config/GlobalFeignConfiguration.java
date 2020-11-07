2ecbd5dbb8d5be8e41c08d2fd310f41c51e6ea55
zhaopengfei <1027926875@qq.com>
2020-11-07 18:25:10 +0800

    fegin

---
+++ src/main/java/com/erudev/contentcenter/config/GlobalFeignConfiguration.java
@@ -0,0 +1,6 @@
+package com.erudev.contentcenter.config;/**
+ * 
+ * @author pengfei.zhao
+ * @date 2020/11/7 16:22
+ */public class GlobalFeignConfiguration {
+}
---
+++ src/main/java/com/erudev/contentcenter/feignclient/UserCenterFeignClient.java
@@ -0,0 +1,6 @@
+package com.erudev.contentcenter.feignclient;/**
+ * 
+ * @author pengfei.zhao
+ * @date 2020/11/7 16:01
+ */public interface UserCenterFeignClient {
+}
